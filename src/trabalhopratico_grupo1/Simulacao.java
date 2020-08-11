d257a9684d97876c907ca160dd5bb095ce61216a
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-08-11 12:35:48 -0300

    Remoção de import desnecessário e marcação de TODO

--- src/trabalhopratico_grupo1/Simulacao.java
+++ src/trabalhopratico_grupo1/Simulacao.java
@@ -1,6 +1,5 @@
 package trabalhopratico_grupo1;
 
-import org.jfree.chart.JFreeChart;
 import java.util.ArrayList;
 
 public class Simulacao {
@@ -11,37 +10,39 @@ public class Simulacao {
     public void simularAgencia() {
         AcessoDados acesso = new AcessoDados();
         acesso.lerArquivo("dadosEntrada.txt");
+        //Debug
         System.out.println("oi");
     }
     
     public static void inserirCliente(Cliente cliente){
         clientes.add(cliente);
     }
-        
+    
+    //TODO
     public int calcularTempoTotal() {
         return 0;
     }
-
+    //TODO
     public double calcularTempoMedio() {
         return 0;
     }
-
+    //TODO
     public double calcularTempoMedioAtendimento() {
         return 0;
     }
-
+    //TODO
     public int calcularNumEventos() {
         return 0;
     }
-
+    //TODO
     public double calcularTempoEsperaMedia() {
         return 0;
     }
-
+    //TODO
     public double calcularTamanhoFilaMedia() {
         return 0;
     }
-
+    //TODO
     public int calcularTamanhoFilaMaximo() {
         return 0;
     }
