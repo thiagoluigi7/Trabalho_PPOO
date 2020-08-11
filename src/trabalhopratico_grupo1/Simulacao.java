8453dce0d37570fdd33116865e62591593a26e8f
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-08-11 18:37:09 -0300

    Documentação da classe Simulação utilizando javadoc

--- src/trabalhopratico_grupo1/Simulacao.java
+++ src/trabalhopratico_grupo1/Simulacao.java
@@ -8,6 +8,15 @@ public class Simulacao {
     private static ArrayList<Cliente> clientes;
     private static ArrayList<Atendente> atendentes;  
 
+    /**
+     * Este método inicia a simulação como uma main.
+     * Ele irá definir o tempo em que o banco inicia
+     * (tempo em que a simulação começa), irá contratar
+     * os funcionários e receber os clientes (ambos a 
+     * partir do arquivo de entrada), irá iniciar o 
+     * atendimento que é o laço principal do programa e
+     * depois irá gerar os relatórios.
+     */
     public void simularAgencia() {
         //Abre a agencia
         tempoAtual = 0;
@@ -19,24 +28,48 @@ public class Simulacao {
         gerarRelatorio();
     }
     
+    /**
+     * Adiciona um cliente na fila de clientes do banco.
+     * Método utilizado durante a leitura do arquivo.
+     * @param cliente
+     */
     public static void inserirCliente(Cliente cliente){
         clientes.add(cliente);
     }
-    
+
+    /**
+     * Adiciona um atendente no banco. Método utilizado 
+     * durante a leitura do arquivo.
+     * @param atendente
+     */
     public static void inserirAtendente(Atendente atendente){
         atendentes.add(atendente);
     }
 
+    /**
+     * Este método retorna o tempo atual para se saber quanto
+     * tempo já se passou desde a abertura do banco.
+     * @return Simulação.tempoAtual
+     */
     public int getTempoAtual() {
         return this.tempoAtual;
     }
 
+    /**
+     * Este método lê o arquivo de entrada. Dele é retirado
+     * quantos funcionários o banco tem, qual sua categoria,
+     * mais os clientes e suas respectivas horas de entrada no 
+     * banco. Os funcionários são então colocados no ArrayList
+     * atendentes e o mesmo ocorre com os clientes que são 
+     * colocados no ArrayList clientes.
+     */
     private void recebeClientes() {
         AcessoDados acesso = new AcessoDados();
         acesso.lerArquivo("dadosEntrada.txt");
     }
 
-    /**Laço principal, executa enquanto ainda tem clientes na fila do banco.
+    /**
+     * Laço principal, executa enquanto ainda tem clientes na fila do banco.
      * O primeiro for simula os clientes na fila. O primeiro cliente da uma passo
      * a frente e procura um atendente livre. Então quando ele encontra ele dá um passo
      * a frente e começa o atendimento.
@@ -54,8 +87,9 @@ public class Simulacao {
         }
     }
 
-    /**Este método irá gerar o relatório com suas estatísticas
-     * e o gráfico.
+    /**
+     * Este método irá gerar o relatório com suas estatísticas
+     * e irá criar uma janela com o gráfico.
      */
     //TODO
     public void gerarRelatorio() {
