e8e24eb6fa4e551f33dde87b75808a9daa633c46
Thiago Luigi <thiagoluigi7@hotmail.com>
2020-08-12 19:46:38 -0300

    Remoção de inserção desnecessária e de comentário desnecessário

--- src/trabalhopratico_grupo1/Principal.java
+++ src/trabalhopratico_grupo1/Principal.java
@@ -1,14 +1,13 @@
 package trabalhopratico_grupo1;
 
-import org.jfree.chart.JFreeChart;
-
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Simulacao simulacao = new Simulacao();
         
         simulacao.simularAgencia();
+
     }
 
 }
